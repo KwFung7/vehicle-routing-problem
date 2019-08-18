061098588c41e3acff75a4de712ac988366cafec
Felix Kwan <kwfxng@gmail.com>
2019-08-18 11:53:17 +0800

    [Formula] update formula for total truck and warehouse

--- helpers/formula.py
+++ helpers/formula.py
@@ -2,19 +2,20 @@
 # -*- coding: utf-8 -*-
 
 
-# Get equipment cost with truck and warehouse purchase cost
-def get_equipment_cost(truck, warehouse_max_capacity, warehouse_days_used):
-    truck_purchase_cost = truck.get_truck_purchase_cost()
-    equipment_cost = truck_purchase_cost + get_warehouse_purchase_cost(warehouse_max_capacity, warehouse_days_used)
+# Get equipment cost with total truck and warehouse purchase cost
+def get_equipment_cost(truck_list, warehouse_list):
+    total_truck_purchase_cost = truck_list.get_total_truck_purchase_cost()
+    total_warehouse_purchase_cost = warehouse_list.get_total_warehouse_purchase_cost()
+    equipment_cost = total_truck_purchase_cost + total_warehouse_purchase_cost
     print('Equipment Cost: {}'.format(equipment_cost))
 
 
-# Get normalized operating cost with truck operating cost, total demand, initial total products and end total products
-def get_normalized_operating_cost(truck, total_demand, total_product_start, total_product_end):
-    truck_operating_cost = truck.get_truck_operating_cost()
-    normalized_operating_cost = truck_operating_cost * total_demand / (total_demand + total_product_start - total_product_end)
+# Get normalized operating cost with total truck operating cost, total demand, initial total products and end total products
+def get_normalized_operating_cost(truck_list, total_demand, total_product_start, total_product_end):
+    total_truck_operating_cost = truck_list.get_total_truck_operating_cost()
+    normalized_operating_cost = total_truck_operating_cost * total_demand / (total_demand + total_product_start - total_product_end)
     print('Normalized Operating Cost [ operating cost: {}, total demand: {}, initial total product: {}, end total product: {} ]: {}'
-          .format(truck_operating_cost, total_demand, total_product_start, total_product_end, normalized_operating_cost))
+          .format(total_truck_operating_cost, total_demand, total_product_start, total_product_end, normalized_operating_cost))
 
 
 # Get total cost with equipment cost and normalized operating cost
