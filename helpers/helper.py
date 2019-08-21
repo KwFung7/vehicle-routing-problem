13c65225fa70fb3f83e3f5ae35fada07ff844330
Felix Kwan <kwfxng@gmail.com>
2019-08-21 11:12:27 +0800

    [Helper] update dataset and helper

--- helpers/helper.py
+++ helpers/helper.py
@@ -5,13 +5,6 @@
 def create_distance_mapping(warehouse_data):
     mapping = {}
     for key in warehouse_data:
-        if '_distance' in key:
+        if 'W' in key or 'D' in key:
             mapping[key] = warehouse_data[key]
     return mapping
-
-
-def convert_name_to_distance_string(name):
-    if name == 'D1':
-        return 'depo_distance'
-    else:
-        return name.replace('W', 'wh') + '_distance'
